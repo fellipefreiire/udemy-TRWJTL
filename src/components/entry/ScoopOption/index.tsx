798400875f8a391ab9dbbe74cde9fa3deb6b09cb
Felipe Freire <fellipefreiire3@gmail.com>
2021-04-07 18:54:54 -0300

    images fix

--- src/components/entry/ScoopOption/index.tsx
+++ src/components/entry/ScoopOption/index.tsx
@@ -14,7 +14,7 @@ const ScoopOption: React.FC<PropsTypes> = ({
     <Col xs={12} sm={6} md={4} lg={3} style={{ textAlign: 'center' }}>
       <img
         style={{ width: '75%' }}
-        src={`http://localhost:3000/${imagePath}`}
+        src={`http://localhost:3030/${imagePath}`}
         alt={`${name} scoop`}
       />
     </Col>
