1c3b1009c4946c7fb9006e48fd77a43075d459b3
Felipe Freire <fellipefreiire3@gmail.com>
2021-04-12 15:57:53 -0300

    button disabled color test

--- src/components/entry/OrderEntry/OrderEntry.test.tsx
+++ src/components/entry/OrderEntry/OrderEntry.test.tsx
@@ -6,8 +6,9 @@ import {
 import OrderEntry from '../OrderEntry'
 import { rest } from 'msw'
 import { server } from '../../../../mocks/server'
+import userEvent from '@testing-library/user-event'
 
-test.only('handles error for scoops and toppings routes', async () => {
+test('handles error for scoops and toppings routes', async () => {
   server.resetHandlers(
     rest.get('http://localhost:3030/scoops', (req, res, ctx) => {
       res(ctx.status(500))
@@ -24,4 +25,22 @@ test.only('handles error for scoops and toppings routes', async () => {
 
     expect(alerts).toHaveLength(2)
   })
+})
+
+test('disable order button if ther are no scoops oredered', async () => {
+  render(<OrderEntry setOrderPhase={jest.fn()} />)
+
+  let orderButton = screen.getByRole('button', { name: /order sundae/i })
+  expect(orderButton).toBeDisabled()
+
+  const vanillaInput = await screen.findByRole('spinbutton', {
+    name: 'Vanilla'
+  })
+  userEvent.clear(vanillaInput)
+  userEvent.type(vanillaInput, '1')
+  expect(orderButton).toBeEnabled()
+
+  userEvent.clear(vanillaInput)
+  userEvent.type(vanillaInput, '0')
+  expect(orderButton).toBeDisabled()
 })
